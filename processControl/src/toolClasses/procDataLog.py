e85672d0e36a4a34db9c8bf3e9ea17351774b827
Alex <alex.leech@talktalk.net>
2016-07-22 22:45:17 +0100

    Completed logging file
    completed logging file however contains one todo

--- processControl/src/toolClasses/procDataLog.py
+++ processControl/src/toolClasses/procDataLog.py
@@ -5,24 +5,39 @@
 @date:   Wed Jul 13 17:21:01 2016
 @rev:    1
 @lang:   Python 2.7
-@deps:   <>
-@desc:   <>
+@deps:   csv, time
+@desc:   class to log process data to a csv file
 """
 
 import csv
 import time
+from yamlImport import yamlImport
+
+#@todo: write logs to their own designted folder
 
 class procDataLog:
     
     def __init__(self):
         """Setup"""
         self.logRun = 0
+        self.headerCfg = yamlImport.importYAML("../../cfg/logHeaders.yaml")
     
     
-    def write(self):
-        """Write values to a log"""
+    def write(self, logData):
+        """Write values to a log
         
-        pass
+        :param logData: list of data to log to the csv file
+        :type logData: list
+        """
+        if self.logRun == 1:
+            if type(logData) != list:
+                logData = [self.__formatTime(), logData]
+            else:
+                logData.insert(0, self.__formatTime())
+            self.csvLog.writerow(logData)
+            self.logFile.flush()
+        else:
+            print("No log currently active")
     
     
     def startLog(self, name=None):
@@ -31,14 +46,20 @@ class procDataLog:
         :param Name: Optional File name. If 'None' sets as date/time
         :type Name: String        
         """
+        if self.logRun == 1:
+            print("A log is already running. Please stop that first")
+            return
         if name == None:
-            fileName = self.formatTime() + ".csv"
+            fileName = self.__formatTimeDate() + ".csv"
         else:
             fileName = str(name) +".csv"
         self.logFile = open(fileName, 'wb')
         self.csvLog = csv.writer(self.logFile,\
                                  delimiter=',',\
                                  quoting=csv.QUOTE_ALL)
+        self.headerCfg["log_headers"].insert(0,"Time")
+        self.csvLog.writerow(self.headerCfg["log_headers"])
+        self.logFile.flush()
         self.logRun = 1
     
     
@@ -47,16 +68,20 @@ class procDataLog:
         self.logFile.close()
         self.logRun = 0
 
-    
-    def formatTime(self):
+
+    def __formatTime(self):
+        """Format the time appropriate for logging"""
+        return time.strftime('%H') + ":" +\
+               time.strftime('%M') + ":" +\
+               time.strftime('%S')
+
+
+    def __formatTimeDate(self):
         """Format the date and time appropriate for a filename"""
-        
         timeNow = time.strftime('%H') + "." +\
                   time.strftime('%M') + "." +\
                   time.strftime('%S')
-        
         date    = time.strftime('%d') + "." +\
                   time.strftime('%m') + "." +\
                   time.strftime('%Y')
-        
         return (timeNow + " " + date)
